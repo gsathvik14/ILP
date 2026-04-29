67dd2ced2bfd8b76e87ccc2b2bf68c4f753f0466
gsathvik14 <gsathvik.sdc@ndmatrix.in>
2026-04-29 10:07:45 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -9,10 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/',(req,res) => {
-    res.send('Hello World!');
+    res.status(200).send('Hello World!');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+export default app;
