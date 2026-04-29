240f54f9d69f352eb567c3c479e08dfee264d20e
gsathvik14 <gsathvik.sdc@ndmatrix.in>
2026-04-29 09:35:44 +0530

    app listening to port

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello World! This is the backend server and testing.");
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World! This is the backend server and testing.");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
