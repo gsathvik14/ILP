2a9eb82fa19000e2f67fdccbdbae038d7d3049c8
gsathvik14 <gsathvik.sdc@ndmatrix.in>
2026-04-29 09:51:54 +0530

    react files

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
+
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Hello World! This is the backend server and testing.");
+app.get('/',(req,res) => {
+    res.send('Hello World!');
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
